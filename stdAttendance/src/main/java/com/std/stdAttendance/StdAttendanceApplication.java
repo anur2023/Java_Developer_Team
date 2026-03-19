f0603db9d911ac05585db102d5e06a6248f45391
Anuruddh <anuruddh209401@gmail.com>
2026-03-19 21:08:14 +0530

    Initialize

---
+++ stdAttendance/src/main/java/com/std/stdAttendance/StdAttendanceApplication.java
@@ -0,0 +1,13 @@
+package com.std.stdAttendance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StdAttendanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StdAttendanceApplication.class, args);
+	}
+
+}
---
+++ stdAttendance/src/test/java/com/std/stdAttendance/StdAttendanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.std.stdAttendance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StdAttendanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
