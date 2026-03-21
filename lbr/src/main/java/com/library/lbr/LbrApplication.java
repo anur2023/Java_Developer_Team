45856d3706efd80d94b576faf03b806b3a3170e2
Anuruddh <anuruddh209401@gmail.com>
2026-03-21 18:19:10 +0530

    Library Backend created

---
+++ lbr/src/main/java/com/library/lbr/LbrApplication.java
@@ -0,0 +1,13 @@
+package com.library.lbr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LbrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LbrApplication.class, args);
+	}
+
+}
